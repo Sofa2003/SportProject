a3e15ec8bf3b2f0b612ee83fac41c374867765a9
Sofa2003 <sborovkova168@gmail.com>
2024-03-26 16:19:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sportproject/Otchet.java
@@ -0,0 +1,14 @@
+package com.example.sportproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Otchet extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otchet);
+    }
+}
